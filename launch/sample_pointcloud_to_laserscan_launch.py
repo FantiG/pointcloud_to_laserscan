4459a96763436678a03458837ca127de23f9e472
Giacomo Fanti <giacomofanti@gmail.com>
2022-10-17 16:38:28 +0200

    Done laserscan merge with right and left scan

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -10,12 +10,12 @@ def generate_launch_description():
             name='scanner', default_value='scanner',
             description='Namespace for sample topics'
         ),
-        Node(
-            package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
-            remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
-            parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 2.0, 'cloud_size': 500}],
-            name='cloud_publisher'
-        ),
+        # Node(
+        #     package='pointcloud_to_laserscan', executable='dummy_pointcloud_publisher',
+        #     remappings=[('cloud', [LaunchConfiguration(variable_name='scanner'), '/cloud'])],
+        #     parameters=[{'cloud_frame_id': 'cloud', 'cloud_extent': 2.0, 'cloud_size': 500}],
+        #     name='cloud_publisher'
+        # ),
         Node(
             package='tf2_ros',
             executable='static_transform_publisher',
@@ -31,16 +31,16 @@ def generate_launch_description():
             remappings=[('cloud_in', [LaunchConfiguration(variable_name='scanner'), '/cloud']),
                         ('scan', [LaunchConfiguration(variable_name='scanner'), '/scan'])],
             parameters=[{
-                'target_frame': 'cloud',
+                'target_frame': 'base_link',
                 'transform_tolerance': 0.01,
-                'min_height': 0.0,
+                'min_height': -0.5,
                 'max_height': 1.0,
-                'angle_min': -1.5708,  # -M_PI/2
-                'angle_max': 1.5708,  # M_PI/2
-                'angle_increment': 0.0087,  # M_PI/360.0
+                'angle_min': -3.1415,  # -M_PI
+                'angle_max': 3.1415,  # M_PI
+                'angle_increment': 0.017,  # 2*M_PI/360.0
                 'scan_time': 0.3333,
-                'range_min': 0.45,
-                'range_max': 4.0,
+                'range_min': 0.01,
+                'range_max': 10.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
             }],
