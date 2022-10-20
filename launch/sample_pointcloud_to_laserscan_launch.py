261d048cd89498ccb3f674a724ea999820305326
FilippoGuarda <filippo.guarda96@gmail.com>
2022-10-20 10:31:20 +0200

    linting

--- launch/sample_pointcloud_to_laserscan_launch.py
+++ launch/sample_pointcloud_to_laserscan_launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
             remappings=[('cloud_in',['/cloud']),
                         ('scan', ['/scan'])],
             parameters=[{
-                'target_frame': 'lidar_frame    ',
+                'target_frame': 'lidar_frame',
                 'transform_tolerance': 0.01,
                 'min_height': -0.5,
                 'max_height': 1.0,
